0d5ebb707171f88834fb3e7e61a7bf5df189acfc
tangbe lucien <lucientangbe45@gmail.com>
2025-11-08 20:29:02 +0100

    chor: add the dynamic header component in house page

--- src/app/(public)/houses/page.tsx
+++ src/app/(public)/houses/page.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { HeaderWithFilterButton } from "@/components/header/HeaderWithFilterButton";
 
 const HousesPage = () => {
-  return <div>Houses Page</div>;
+  return (
+    <div className="page">
+      <div className="px-1">
+        <HeaderWithFilterButton title="Houses" hasFilter={true} />
+      </div>
+      <div className="p-sync">Houses Page</div>
+    </div>
+  );
 };
 
 export default HousesPage;
